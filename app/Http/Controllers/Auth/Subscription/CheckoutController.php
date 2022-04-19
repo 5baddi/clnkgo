c0b91bdd71e175d567374fa3ba787b7891137a4f
Mohamed Baddi <project@baddi.info>
2022-04-19 19:13:08 +0200

    Re-use Stripe

--- app/Http/Controllers/Auth/Subscription/CheckoutController.php
+++ app/Http/Controllers/Auth/Subscription/CheckoutController.php
@@ -11,6 +11,7 @@ namespace BADDIServices\SourceeApp\Http\Controllers\Auth\Subscription;
 use Illuminate\Http\Response;
 use BADDIServices\SourceeApp\Models\Pack;
 use BADDIServices\SourceeApp\Services\PackService;
+use BADDIServices\SourceeApp\Domains\StripeService;
 use BADDIServices\SourceeApp\Http\Controllers\DashboardController;
 
 class CheckoutController extends DashboardController
@@ -18,11 +19,14 @@ class CheckoutController extends DashboardController
     /** @var PackService */
     private $packService;
 
-    public function __construct(PackService $packService)
+    private StripeService $stripeService;
+
+    public function __construct(PackService $packService, StripeService $stripeService)
     {
         parent::__construct();
 
         $this->packService = $packService;
+        $this->stripeService = $stripeService;
     }
 
     public function __invoke(string $id)
@@ -30,12 +34,16 @@ class CheckoutController extends DashboardController
         $pack = $this->packService->findById($id);
         abort_unless($pack instanceof Pack, Response::HTTP_NOT_FOUND);
 
-        return view(
-            'dashboard.plan.checkout', 
-            [
-                'title' => 'Checkout',
-                'pack'  => $pack
-            ]
-        );
+        $checkoutUrl = $this->stripeService->getCheckoutSessionUrl($pack, $this->user);
+
+        return redirect()->to($checkoutUrl, Response::HTTP_SEE_OTHER);
+
+        // return view(
+        //     'dashboard.plan.checkout', 
+        //     [
+        //         'title' => 'Checkout',
+        //         'pack'  => $pack
+        //     ]
+        // );
     }
 }
