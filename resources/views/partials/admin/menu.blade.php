4fb728bcdbc67d5139573e0a2d4a725c760c00ff
Mohamed Baddi <project@baddi.info>
2022-04-21 23:34:38 +0200

    WIP - UI

--- resources/views/partials/admin/menu.blade.php
+++ resources/views/partials/admin/menu.blade.php
@@ -10,16 +10,18 @@
       </h1>
       <div class="collapse navbar-collapse" id="navbar-menu">
         <ul class="navbar-nav pt-lg-3">
-          <li class="nav-item {{ request()->routeIs(['dashboard', 'dashboard.requests', 'dashboard.requests.*']) ? 'active' : '' }}">
-            <a class="nav-link" href="{{ route('dashboard') }}">
+          <li class="nav-item {{ request()->routeIs('admin') ? 'active' : '' }}">
+            <a class="nav-link" href="{{ route('admin') }}">
                 <span class="nav-link-icon d-md-none d-lg-inline-block">
-                  <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-messages" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                    <path d="M21 14l-3 -3h-7a1 1 0 0 1 -1 -1v-6a1 1 0 0 1 1 -1h9a1 1 0 0 1 1 1v10"></path>
-                    <path d="M14 15v2a1 1 0 0 1 -1 1h-7l-3 3v-10a1 1 0 0 1 1 -1h2"></path>
-                  </svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-chart-bar" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                        <rect x="3" y="12" width="6" height="8" rx="1"></rect>
+                        <rect x="9" y="8" width="6" height="12" rx="1"></rect>
+                        <rect x="15" y="4" width="6" height="16" rx="1"></rect>
+                        <line x1="4" y1="20" x2="18" y2="20"></line>
+                    </svg>
                 </span>
-                <span class="nav-link-title">Queries</span>
+                <span class="nav-link-title">Analytics</span>
             </a>
           </li>
           <li class="nav-item {{ request()->routeIs('dashboard.keywords') ? 'active' : '' }}">
