f4a3acb43c3c5a3500fd906e1e6bd27ec4a77956
5baddi <project@baddi.info>
2022-05-21 16:02:47 +0100

    fix: typo

--- resources/views/dashboard/requests/show.blade.php
+++ resources/views/dashboard/requests/show.blade.php
@@ -202,7 +202,7 @@
                         <circle cx="12" cy="11" r="3"></circle>
                         <path d="M17.657 16.657l-4.243 4.243a2 2 0 0 1 -2.827 0l-4.244 -4.243a8 8 0 1 1 11.314 0z"></path>
                     </svg>&nbsp;
-                    <span title="Published at">{{ $tweet->author->location }}</span>
+                    <span title="Location">{{ $tweet->author->location }}</span>
                     @endif
                     @if($tweet->author->website)
                     <svg style="margin-left: 2rem !important;" xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-world" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
