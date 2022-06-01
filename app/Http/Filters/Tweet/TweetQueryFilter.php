0ee00dc2ab4d4deabda715add9826d7c17fd0d3a
5baddi <project@baddi.info>
2022-06-01 15:00:16 +0100

    debug

--- app/Http/Filters/Tweet/TweetQueryFilter.php
+++ app/Http/Filters/Tweet/TweetQueryFilter.php
@@ -114,10 +114,10 @@ class TweetQueryFilter extends QueryFilter
     {
         $keywords = $user->getKeywords() ?? [];
 
-        $this->builder
-            ->whereDate(Tweet::PUBLISHED_AT_COLUMN, '>', Carbon::now()->subDays(10))
-            ->whereDate(Tweet::DUE_AT_COLUMN, '>=', Carbon::now())
-            ->orWhereNull(Tweet::DUE_AT_COLUMN);
+        // $this->builder
+        //     ->whereDate(Tweet::PUBLISHED_AT_COLUMN, '>', Carbon::now()->subDays(10))
+        //     ->whereDate(Tweet::DUE_AT_COLUMN, '>=', Carbon::now())
+        //     ->orWhereNull(Tweet::DUE_AT_COLUMN);
 
         foreach($keywords as $index => $keyword) {
             if ($index === 0) {
