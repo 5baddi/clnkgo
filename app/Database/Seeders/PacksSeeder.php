bf09182dbb096bd57439d90dd1282ba910bb09fa
5baddi <project@baddi.info>
2022-06-08 14:21:58 +0100

    update default pack

--- app/Database/Seeders/PacksSeeder.php
+++ app/Database/Seeders/PacksSeeder.php
@@ -22,42 +22,42 @@ class PacksSeeder extends Seeder
     {
         Pack::create([
             'name'          => 'The plan',
-            'price'         => 25,
+            'price'         => 12,
             'type'          => Pack::RECURRING_TYPE,
             'is_popular'    => true,
-            'trial_days'    => 7,
+            'trial_days'    => 3,
             'features'      => [
                 [
-                    'key'       =>  Pack::ALL_REQUESTS_ACCESS,
-                    'name'      =>  'Access to all Journo Requests',
-                    'enabled'   =>  true
+                    'key'       => Pack::ALL_REQUESTS_ACCESS,
+                    'name'      => 'Access to all Journo Requests',
+                    'enabled'   => true
                 ],
                 [
-                    'key'       =>  Pack::INSTANT_REQUEST_NOTIFICATIONS,
-                    'name'      =>  'Instant Journo Request notifications',
-                    'enabled'   =>  true
+                    'key'       => Pack::INSTANT_REQUEST_NOTIFICATIONS,
+                    'name'      => 'Instant Journo Request notifications',
+                    'enabled'   => true
                 ],
                 [
-                    'key'       =>  Pack::KEYWORDS,
-                    'name'      =>  '30 Keywords - More keywords = A greater chance of receiving relevant Journo Request notifications straight to your inbox',
-                    'enabled'   =>  true,
+                    'key'       => Pack::KEYWORDS,
+                    'name'      => 'Up to 30 Keywords',
+                    'enabled'   => true,
                     'limit'     => 30
                 ],
                 [
-                    'key'       =>  Pack::CANNED_RESPONSES,
-                    'name'      =>  '10 Templates - Save time by storing commonly used responses to Journo Requests',
-                    'enabled'   =>  true,
+                    'key'       => Pack::CANNED_RESPONSES,
+                    'name'      => 'Up to 10 Templates',
+                    'enabled'   => true,
                     'limit'     => 10
                 ],
                 [
-                    'key'       =>  Pack::SUPPORT,
-                    'name'      =>  'live chat support',
-                    'enabled'   =>  true,
+                    'key'       => Pack::SUPPORT,
+                    'name'      => '24/7 support',
+                    'enabled'   => true,
                 ],
                 [
-                    'key'       =>  Pack::CANCEL_ANYTIME,
-                    'name'      =>  'Cancel Anytime',
-                    'enabled'   =>  true,
+                    'key'       => Pack::CANCEL_ANYTIME,
+                    'name'      => 'Cancel Anytime',
+                    'enabled'   => true,
                 ],
             ]
         ]);
