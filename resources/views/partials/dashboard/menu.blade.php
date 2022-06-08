f083bc7b6ffbd30372b2e90c7012fa6f10ee3ecd
5baddi <project@baddi.info>
2022-06-08 08:15:06 +0100

    fix: remove unused links

--- resources/views/partials/dashboard/menu.blade.php
+++ resources/views/partials/dashboard/menu.blade.php
@@ -134,12 +134,12 @@
         <div class="row mb-4">
             <div class="col-12">
                 <div class="col-auto align-self-center mt-1 text-center">
-                    <a href="{{ env('SUPPORT_URL', '#') }}" target="_blank" class="btn btn-icon btn-without-bg w-100">
+                    {{-- <a href="{{ env('SUPPORT_URL', '#') }}" target="_blank" class="btn btn-icon btn-without-bg w-100">
                       Report A Bug
                     </a>
                     <a href="{{ env('SUPPORT_URL', '#') }}" target="_blank" class="btn btn-icon btn-without-bg w-100">
                       Support
-                    </a>
+                    </a> --}}
                     <a href="{{ route('signout') }}" class="btn btn-icon w-100 mt-3">
                       <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-logout" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                         <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
