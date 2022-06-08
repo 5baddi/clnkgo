99feabc67fde500d8db9571b41d7711e187d1d5e
5baddi <project@baddi.info>
2022-06-08 21:02:55 +0100

    fix js

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -89,8 +89,7 @@
       });
 
       $('.back-top').click(function() {
-          $('html, body').animate({scrollTop : 0}, 800);
-          return false;
+          $('html, body').animate({scrollTop : 0}, 300);
       });
     });
 
