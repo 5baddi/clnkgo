ff3be989b3f7851c0cf698ea955d07ab65ad9784
5baddi <project@baddi.info>
2022-06-08 20:49:15 +0100

    init back to top

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -64,6 +64,14 @@
         </footer>
       </div>
     </div>
+
+    <a href="javascript:void(0)" class="btn btn-clnkgo w-100 btn-icon" aria-label="Back to top">
+      <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-chevron-up" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+        <polyline points="6 15 12 9 18 15"></polyline>
+      </svg>
+    </a>
+
     <script src="{{ asset('assets/libs/jquery/jquery.min.js') }}"></script>
     @yield('scripts')
     <script src="{{ asset('assets/js/tabler.min.js') }}"></script>
