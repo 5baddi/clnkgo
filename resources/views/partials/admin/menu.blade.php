d3f6994dca82d9e298f71b0adfb3b8b2a3217735
5baddi <project@baddi.info>
2022-06-08 19:06:11 +0100

    wip UI

--- resources/views/partials/admin/menu.blade.php
+++ resources/views/partials/admin/menu.blade.php
@@ -54,7 +54,7 @@
         <div class="row mb-4">
             <div class="col-12">
                 <div class="col-auto align-self-center mt-1 text-center">
-                    <a href="{{ route('signout') }}" class="btn btn-icon w-100 mt-3">
+                    <a href="{{ route('signout') }}" class="btn btn-clnkgo btn-icon w-100 mt-3">
                       <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-logout" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                         <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                         <path d="M14 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2"></path>
--- resources/views/partials/dashboard/menu.blade.php
+++ resources/views/partials/dashboard/menu.blade.php
@@ -140,7 +140,7 @@
                     {{-- <a href="{{ env('SUPPORT_URL', '#') }}" target="_blank" class="btn btn-icon btn-without-bg w-100">
                       Support
                     </a> --}}
-                    <a href="{{ route('signout') }}" class="btn btn-icon w-100 mt-3">
+                    <a href="{{ route('signout') }}" class="btn btn-clnkgo btn-icon w-100 mt-3">
                       <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-logout" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                         <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                         <path d="M14 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2"></path>
