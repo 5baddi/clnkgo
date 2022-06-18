dc3e2c6179857eea74a863950bbcf6ba251e3b82
5baddi <project@baddi.info>
2022-06-18 19:36:03 +0100

    wip

--- database/migrations/2022_06_11_134710_add_charge_id_column_to_subscriptions_table.php
+++ database/migrations/2022_06_11_134710_add_charge_id_column_to_subscriptions_table.php
@@ -14,7 +14,7 @@ class AddChargeIdColumnToSubscriptionsTable extends Migration
     public function up()
     {
         Schema::table('subscriptions', function (Blueprint $table) {
-            $table->string('charge_id')->nullable();
+            $table->string('charge_id')->nullable()->after('status');
         });
     }
 
