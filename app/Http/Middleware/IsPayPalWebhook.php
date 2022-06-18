6333cb7119b7f7c8e36255ce326f5b230289978f
5baddi <project@baddi.info>
2022-06-18 20:45:43 +0100

    fix: debug middleware

--- app/Http/Middleware/IsPayPalWebhook.php
+++ app/Http/Middleware/IsPayPalWebhook.php
@@ -8,11 +8,13 @@
 
 namespace BADDIServices\ClnkGO\Http\Middleware;
 
+use BADDIServices\ClnkGO\AppLogger;
 use BADDIServices\ClnkGO\Domains\PayPalService;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Arr;
+use Throwable;
 
 class IsPayPalWebhook
 {
@@ -29,22 +31,30 @@ class IsPayPalWebhook
      */
     public function handle(Request $request, Closure $next)
     {
-        if (! Arr::has($request->header(), PayPalService::HEADERS)) {
-            abort(Response::HTTP_BAD_GATEWAY);
+        try {
+            if (! Arr::has($request->header(), PayPalService::HEADERS)) {
+                abort(Response::HTTP_BAD_GATEWAY);
+            }
+            
+            if (! Arr::has($request->input(), ['event_type', 'id'])) {
+                abort(Response::HTTP_BAD_REQUEST);
+            }
+    
+            if (! $this->payPalService->verifySignature(
+                Arr::only($request->header(), PayPalService::HEADERS),
+                $request->input('event_type'),
+                $request->input('id')
+            )) {
+                abort(Response::HTTP_UNAUTHORIZED);
+            }
+    
+            return $next($request);
+        } catch (Throwable $e) {
+            AppLogger::error(
+                $e,
+                'middleware:is-paypal-webhook',
+                ['payload' => $request->all(), 'headers' => $request->header()]
+            );
         }
-        
-        if (! Arr::has($request->input(), ['event_type', 'id'])) {
-            abort(Response::HTTP_BAD_REQUEST);
-        }
-
-        if (! $this->payPalService->verifySignature(
-            Arr::only($request->header(), PayPalService::HEADERS),
-            $request->input('event_type'),
-            $request->input('id')
-        )) {
-            abort(Response::HTTP_UNAUTHORIZED);
-        }
-
-        return $next($request);
     }
 }
