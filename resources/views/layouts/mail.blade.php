fa8c7f303b35008c69d11eebd0bcc847beddcb98
5baddi <project@baddi.info>
2022-06-19 18:38:33 +0100

    fix: disable support section

--- resources/views/layouts/mail.blade.php
+++ resources/views/layouts/mail.blade.php
@@ -181,9 +181,9 @@
                     </table>
                 </td>
             </tr>
-            @if(! isset($showSocialSection) || $showSocialSection !== false)
+            {{-- @if(! isset($showSocialSection) || $showSocialSection !== false)
             @include('partials.mail.sections.social')
-            @endif
+            @endif --}}
             <tr>
                 <td bgcolor="#f4f4f4" align="center" style="padding: 0px 10px 0px 10px;">
                     <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
