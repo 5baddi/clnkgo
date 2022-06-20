d2ff83bd15c68eb7d12de700d4cc9ec70a4b2312
5baddi <project@baddi.info>
2022-06-20 08:05:06 +0100

    debug

--- routes/api/webhooks/paypal.php
+++ routes/api/webhooks/paypal.php
@@ -9,7 +9,7 @@
 use Illuminate\Support\Facades\Route;
 use BADDIServices\ClnkGO\Http\Controllers\Webhooks\PayPal\HandlePayPalWebhookController;
 
-Route::middleware(['api', 'is.paypal-webhook'])
+Route::middleware(['api'])
     ->prefix('webhooks/paypal')
     ->name('webhooks.paypal')
     ->group(function() {
