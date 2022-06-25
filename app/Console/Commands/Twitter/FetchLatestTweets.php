ef24aa6e33c0691957ac7b72a3b5989a53374d66
5baddi <project@baddi.info>
2022-06-25 21:48:10 +0100

    fix: return type

--- app/Console/Commands/Twitter/FetchLatestTweets.php
+++ app/Console/Commands/Twitter/FetchLatestTweets.php
@@ -86,7 +86,7 @@ class FetchLatestTweets extends Command
         $this->info("Done fetching latest tweets");
     }
 
-    private function fetchTweets(string $hashtag, string $startTimeOption, ?string $nextToken = null): callable
+    private function fetchTweets(string $hashtag, string $startTimeOption, ?string $nextToken = null)
     {
         $tweets = $this->twitterService->fetchTweetsByHashtags($hashtag, $startTimeOption, $nextToken);
 
