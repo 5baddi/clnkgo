d5c5c85da76a45ade72164b33b846d8a9a01c021
5baddi <project@baddi.info>
2022-06-26 19:31:28 +0100

    fix: to

--- resources/views/layouts/mail.blade.php
+++ resources/views/layouts/mail.blade.php
@@ -5,7 +5,6 @@
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <!--email_off--> {{ $to }} <!--/email_off-->
         <link rel="icon" type="image/png" href="{{ asset('assets/img/logo.mini.png') }}"/>
         <style type="text/css">
             @media screen {
