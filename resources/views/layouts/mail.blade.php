e9d733b691f8ab1a7645f9f16ff0e1d343582d1e
5baddi <project@baddi.info>
2022-06-27 23:34:47 +0100

    fix mail

--- resources/views/layouts/mail.blade.php
+++ resources/views/layouts/mail.blade.php
@@ -109,44 +109,44 @@
         </div>
         <table border="0" cellpadding="0" cellspacing="0" width="100%">
             <tr>
-                <td bgcolor="#f4f6fa" align="center">
-                    <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
-                        <tr>
-                            <td align="center" valign="top" style="padding: 20px 10px 20px 10px;"> </td>
-                        </tr>
-                    </table>
-                </td>
-            </tr>
-            <tr>
-                <td bgcolor="#f4f6fa" align="center" style="padding: 0px;">
-                    <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
-                        <tr>
-                            <td bgcolor="#04AF90" align="center" valign="top"
-                                style="padding: 20px 20px 20px 20px; border-radius: 4px 4px 0px 0px; color: #111111; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 48px; font-weight: 400; letter-spacing: 4px; line-height: 48px;">
-                                <a href="{{ route('home') }}" target="_blank">
-                                    <img src="{{ asset('assets/img/logo-white.png') }}" width="200" height="auto"
-                                        style="display: block; border: 0px;" />
-                                </a>
-                            </td>
-                        </tr>
-                    </table>
-                </td>
-            </tr>
-            <tr>
-                <td bgcolor="#f4f4f4" align="center" style="padding: 0px;">
-                    <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
-                        <tr>
-                            <td bgcolor="#04AF90" align="left"
-                                style="padding: 10px 30px 10px 30px; color: #666666; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;"></td>
-                        </tr>
-                    </table>
-                </td>
-            </tr>
-            <tr>
                 <td bgcolor="#f4f4f4" align="center" style="padding: 0px 10px 0px 10px;">
                     <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
                         <tr>
-                            <td style="padding: 10px 0px 0px 0px"></td>
+                            <td bgcolor="#f4f6fa" align="center">
+                                <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
+                                    <tr>
+                                        <td align="center" valign="top" style="padding: 20px 10px 20px 10px;"> </td>
+                                    </tr>
+                                </table>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td bgcolor="#f4f6fa" align="center" style="padding: 0px;">
+                                <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
+                                    <tr>
+                                        <td bgcolor="#04AF90" align="center" valign="top"
+                                            style="padding: 20px 20px 20px 20px; border-radius: 4px 4px 0px 0px; color: #111111; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 48px; font-weight: 400; letter-spacing: 4px; line-height: 48px;">
+                                            <a href="{{ route('home') }}" target="_blank">
+                                                <img src="{{ asset('assets/img/logo-white.png') }}" width="200" height="auto"
+                                                    style="display: block; border: 0px;" />
+                                            </a>
+                                        </td>
+                                    </tr>
+                                </table>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td bgcolor="#f4f4f4" align="center" style="padding: 0px;">
+                                <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
+                                    <tr>
+                                        <td bgcolor="#04AF90" align="left"
+                                            style="padding: 10px 30px 10px 30px; color: #666666; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;"></td>
+                                    </tr>
+                                </table>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td bgcolor="#fff" style="padding: 10px 0px 0px 0px"></td>
                         </tr>
                         <tr>
                             <td bgcolor="#fff" align="left"
@@ -155,7 +155,7 @@
                             </td>
                         </tr>
                         <tr>
-                            <td style="padding: 10px 0px 0px 0px"></td>
+                            <td bgcolor="#fff" style="padding: 10px 0px 0px 0px"></td>
                         </tr>
                         @yield('content')
                         @if(! isset($showHelpSection) || $showHelpSection !== false)
