fa8082f83b6a8622d62ac26000c50967c9a2ed3a
5baddi <project@baddi.info>
2022-07-02 18:57:58 +0100

    fix: load more url query

--- resources/views/partials/dashboard/scripts/loadmore-tweets.blade.php
+++ resources/views/partials/dashboard/scripts/loadmore-tweets.blade.php
@@ -95,7 +95,7 @@
           ++page;
   
           $.ajax({
-              url: `{{ route('dashboard.paginate.tweets') }}?{{ count(Request()->query()) === 0 ? '' : http_build_query(Request()->query()) . '&' }}page=${page}`,
+              url: `{{ route('dashboard.paginate.tweets') }}?page=${page}&{{ http_build_query(Request()->query()) }}`,
               type: 'get',
               success: function(response){
                 $('.custom-loader').css('display', 'none');
