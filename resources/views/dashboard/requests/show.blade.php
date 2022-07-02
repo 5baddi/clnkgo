68a97d17256f2a5d2e86e22ffd650d672fe9faaa
5baddi <project@baddi.info>
2022-07-02 18:49:55 +0100

    wip UI

--- resources/views/dashboard/requests/show.blade.php
+++ resources/views/dashboard/requests/show.blade.php
@@ -41,7 +41,16 @@
                   @endif
                 </div>
                 <div class="card-actions">
-                    <a class="btn btn-clnkgo btn-xs" href="{{ route('dashboard', ['author' => $tweet->getAuthorId()]) }}">Show more queries</a>
+                    <a class="btn btn-clnkgo btn-xs" href="{{ route('dashboard', ['author' => $tweet->getAuthorId()]) }}">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-dots-circle-horizontal" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                            <circle cx="12" cy="12" r="9"></circle>
+                            <line x1="8" y1="12" x2="8" y2="12.01"></line>
+                            <line x1="12" y1="12" x2="12" y2="12.01"></line>
+                            <line x1="16" y1="12" x2="16" y2="12.01"></line>
+                        </svg>
+                        &nbsp;Show more queries
+                    </a>
 
                     @if($featureService->isEnabled(\BADDIServices\ClnkGO\App::MARK_AS_ANSWERED_FEATURE) && (! $answer || ! $answer->isAnswered()))
                     <form action="{{ route('dashboard.requests.answered', ['id' => $tweet->getId()]) }}" method="POST">
