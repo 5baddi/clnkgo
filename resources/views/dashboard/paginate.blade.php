edc9a4496e10099ae66606aa445abf6e5408eb7e
5baddi <project@baddi.info>
2022-07-05 23:23:40 +0100

    wip new feature

--- resources/views/dashboard/paginate.blade.php
+++ resources/views/dashboard/paginate.blade.php
@@ -3,6 +3,43 @@
     @continue
     @endif
     <div class="col-12">
+        @if($user->isSuperAdmin())
+        <div class="card">
+            <div class="card-header">
+                <div>
+                    <div class="row align-items-center">
+                      <div class="col-auto">
+                        <span class="avatar rounded" style="background-image: url({{ $tweet->author->profile_image_url ?? asset('assets/img/default_avatar.png') }})"></span>
+                      </div>
+                      <div class="col">
+                        <div class="card-title">{{ $tweet->author->name }}</div>
+                        <div class="card-subtitle">{{ '@' . $tweet->author->username }}</div>
+                      </div>
+                    </div>
+                </div>
+                <div class="card-actions btn-actions">
+                    <a href="#" class="btn-action"><!-- Download SVG icon from http://tabler-icons.io/i/refresh -->
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M20 11a8.1 8.1 0 0 0 -15.5 -2m-.5 -4v4h4"></path><path d="M4 13a8.1 8.1 0 0 0 15.5 2m.5 4v-4h-4"></path></svg>
+                    </a>
+                    <a href="#" class="btn-action"><!-- Download SVG icon from http://tabler-icons.io/i/chevron-up -->
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><polyline points="6 15 12 9 18 15"></polyline></svg>
+                    </a>
+                    <a href="#" class="btn-action"><!-- Download SVG icon from http://tabler-icons.io/i/dots-vertical -->
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><circle cx="12" cy="12" r="1"></circle><circle cx="12" cy="19" r="1"></circle><circle cx="12" cy="5" r="1"></circle></svg>
+                    </a>
+                    <a href="#" class="btn-action"><!-- Download SVG icon from http://tabler-icons.io/i/x -->
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
+                    </a>
+                </div>
+            </div>
+            <div class="card-body p-0">
+                <svg xmlns="http://www.w3.org/2000/svg" class="w-100" preserveAspectRatio="none" width="400" height="200" viewBox="0 0 400 200" stroke="var(--tblr-border-color, #b8cef1)">
+                    <line x1="0" y1="0" x2="400" y2="200"></line>
+                    <line x1="0" y1="200" x2="400" y2="0"></line>
+                </svg>
+            </div>
+        </div>
+        @else
         <a class="card card-link" href="{{ route('dashboard.requests.show', ['id' => $tweet->getId()]) }}">
             <div class="card-body">
                 <div class="row">
@@ -41,5 +78,6 @@
                 </div>
             </div>
         </a>
+        @endif
     </div>
 @endforeach
