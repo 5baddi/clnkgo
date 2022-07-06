ae694b6a748eb6610b959b7cb0d9db280d918fda
5baddi <project@baddi.info>
2022-07-06 15:01:33 +0100

    fix js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,10 @@
 require('./bootstrap');
 
-window.Vue = require("vue").default;
+window.Vue = require('vue').default;
+window.VueResource = require('vue-resource');
+window.Vue.use(window.VueResource);
 
-Vue.component('statistic-card', require('./components/statisticCard.vue').default);
+Vue.component('statistic-card', require('./components/statisticCard.vue'));
 
 const app = new Vue({
     el: '#app',
