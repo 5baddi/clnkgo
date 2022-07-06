697750217cf356bff8b4d80002b08f77a50569e5
5baddi <project@baddi.info>
2022-07-06 08:12:47 +0100

    disable journalist area

--- routes/web/journalist/stats.php
+++ routes/web/journalist/stats.php
@@ -12,6 +12,6 @@ use BADDIServices\ClnkGO\Http\Controllers\Dashboard\IndexController;
 Route::middleware(['auth', 'has.subscription'])
     ->prefix('journalist')
     ->group(function() {
-        Route::get('/', IndexController::class)->name('journalist');
-        Route::post('/', IndexController::class)->name('journalist.filtered');
+        // Route::get('/', IndexController::class)->name('journalist');
+        // Route::post('/', IndexController::class)->name('journalist.filtered');
     });
