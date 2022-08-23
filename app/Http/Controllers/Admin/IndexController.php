27601f61d6f6cb6aa4e0578cd5eddf727ce59fb7
5baddi <project@baddi.info>
2022-08-23 20:30:20 +0100

    feat: email count

--- app/Http/Controllers/Admin/IndexController.php
+++ app/Http/Controllers/Admin/IndexController.php
@@ -9,11 +9,28 @@
 namespace BADDIServices\ClnkGO\Http\Controllers\Admin;
 
 use BADDIServices\ClnkGO\Http\Controllers\AdminController;
+use BADDIServices\ClnkGO\Models\Marketing\MailingList;
 
 class IndexController extends AdminController
 {
     public function __invoke()
     {
-        return $this->render('admin.index');
+        $verifiedEmails = MailingList::query()
+            ->where(MailingList::IS_ACTIVE_COLUMN, 1)
+            ->where(MailingList::IS_UNSUBSCRIBED_COLUMN, 0)
+            ->count();
+            
+        $unverifiedEmails = MailingList::query()
+            ->where(MailingList::IS_ACTIVE_COLUMN, 0)
+            ->where(MailingList::IS_UNSUBSCRIBED_COLUMN, 1)
+            ->count();
+
+        return $this->render(
+            'admin.index',
+            [
+                'verifiedEmails'   => $verifiedEmails,
+                'unverifiedEmails' => $unverifiedEmails,
+            ]
+        );
     }
 }
