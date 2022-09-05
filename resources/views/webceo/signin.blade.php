0d3f44634cfa50cfb1667d70cba5271a08b5d61f
5baddi <project@baddi.info>
2022-09-05 21:21:52 +0100

    style

--- resources/views/webceo/signin.blade.php
+++ resources/views/webceo/signin.blade.php
@@ -5,6 +5,8 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Titillium+Web:wght@200;300;400;600;700&display=swap" rel="stylesheet">
     <title>seokits - sign in</title>
     <style>
         .form-bg {
@@ -173,11 +175,6 @@
                             <a href="#" class="fa fa-question-circle"></a>
                         </div>
                         <div class="form-group">
-                            <div class="main-checkbox">
-                                <input type="checkbox" value="None" id="checkbox1" name="check">
-                                <label for="checkbox1"></label>
-                            </div>
-                            <span class="text">Remember me</span>
                             <button type="submit" class="btn btn-default">log in</button>
                         </div>
                     </form>
